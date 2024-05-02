fe5729e0c5dc943cb84e905ed26f71676a7feaa1
solana-memetoken-starterkit <166315812+solana-memetoken-starterkit@users.noreply.github.com>
2024-05-02 18:23:07 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main
-      className="items-center justify-center w-full h-screen flex bg-gradient-to-b from-black to-transparent bg-no-repeat bg-cover bg-center"
-      style={{
-        backgroundImage: "url(https://solanaexplore.com/claim/bg.jpeg)",
-      }}
-    >
+    <main className="items-center justify-center w-full h-screen flex bg-[url('/bg.jpeg')] bg-no-repeat bg-cover bg-center">
       <div className="mt-4 flex flex-col items-center">
         <WalletMButton />
         <Airdrop />
